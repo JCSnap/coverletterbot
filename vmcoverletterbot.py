dca1c3e73b6316e1f499763ab1a271a22e6f10b5
JCSnap <111076731+JCSnap@users.noreply.github.com>
2023-01-16 06:45:33 +0800

    Update vmcoverletterbot.py

--- vmcoverletterbot.py
+++ vmcoverletterbot.py
@@ -47,7 +47,7 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 token1 = '5943071283:AAHBvhDH0GS7FkcJoOHKfZHR3-AsP_TQai8'
-openai.api_key = "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"
+openai.api_key = "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"
 ## openai.api_key = os.environ['OPENAI_KEY']
 
 
